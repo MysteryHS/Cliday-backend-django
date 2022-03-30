a5fd7e6b796b31cbb415a78de4043fb63a29c762
Mateo Ducastel <38300124+MysteryHS@users.noreply.github.com>
2022-03-30 20:24:03 +0200

    Modif Answer > Question Required

--- api/serializers.py
+++ api/serializers.py
@@ -46,10 +46,6 @@ class ChoiceSerializer(ModelSerializer):
         model = Choice
         fields = ['is_correct', 'account', 'question']
         
-        extra_kwargs = {
-            'question': {'required': True},
-        }
-
 class ChoiceCompleteSerializer(ModelSerializer):
     
     class Meta:
