a7536c2ca28020dd2a9a35301a77b8969c13ac5f
Mateo Ducastel <38300124+MysteryHS@users.noreply.github.com>
2022-04-23 17:24:27 +0200

    correct handle args job

--- api/management/commands/update_questions.py
+++ api/management/commands/update_questions.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Command(BaseCommand):
     help = "Django Daily Job"
 
-    def handle(self):
+    def handle(self, *args, **options):
         Question.objects.order_by('-date_selected')[:5].update(date_selected=timezone.now()) 
         #default timezone is utc in heroku
         
